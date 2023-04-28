ae187de707fb7874abfa92c40b54607614ae63e3
Karlos Silva <karlossilva194@gmail.com>
2023-04-28 01:56:28 -0300

    feat: create controller to return all vehicles

--- packages/server/src/http/controllers/vehicles/routes.ts
+++ packages/server/src/http/controllers/vehicles/routes.ts
@@ -1,10 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
 import { deleteVehicle } from './delete-vehicle'
+import { list } from './list'
 
 
 export async function vehiclesRoutes(app: FastifyInstance) {
   app.post('/vehicles', create)
+
+  app.get('/vehicles/:fleetId', list)
 
   app.delete('/vehicles/:chassisId', deleteVehicle)
 }
